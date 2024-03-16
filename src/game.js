df732a32439a4a5dbc79d2d95640ee5772380bd6
Yasin Akgül <62186511+yasinakgulbp@users.noreply.github.com>
2024-03-17 01:58:01 +0300

    matter js eklendi

--- src/game.js
+++ src/game.js
@@ -1,5 +1,6 @@
 import gsap, { Power0 } from "gsap";
 import { Container, Sprite } from "pixi.js";
+import Matter from "matter-js";
 import { GAME_HEIGHT, GAME_WIDTH } from ".";
 
 export default class Game extends Container {
@@ -15,7 +16,7 @@ export default class Game extends Container {
     sprite.anchor.set(0.5);
     sprite.scale.set(0.5);
     this.addChild(sprite);
-    sprite.x = GAME_WIDTH * 0.5;
+    sprite.x = GAME_WIDTH * 0.5;  
     sprite.y = GAME_HEIGHT * 0.5;
 
     gsap.to(sprite, {
@@ -27,5 +28,38 @@ export default class Game extends Container {
       yoyo: true,
       ease: "sine.easeInOut",
     })
+
+    // module aliases
+var Engine = Matter.Engine,
+Render = Matter.Render,
+Runner = Matter.Runner,
+Bodies = Matter.Bodies,
+Composite = Matter.Composite;
+
+// create an engine
+var engine = Engine.create();
+
+// create a renderer
+var render = Render.create({
+element: document.body,
+engine: engine
+});
+
+// create two boxes and a ground
+var boxA = Bodies.rectangle(400, 200, 80, 80);
+var boxB = Bodies.rectangle(450, 50, 80, 80);
+var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+// add all of the bodies to the world
+Composite.add(engine.world, [boxA, boxB, ground]);
+
+// run the renderer
+Render.run(render);
+
+// create runner
+var runner = Runner.create();
+
+// run the engine
+Runner.run(runner, engine);
   }
 }
